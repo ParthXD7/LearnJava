77c66959fbc3fdc671e3edfba6fa560ab405451d
XD777 <67903668+XD777OP@users.noreply.github.com>
2024-02-12 00:55:24 +0530

    Added College CHO Codes

---
+++ src/CollegeNameAnalyzer.java
@@ -0,0 +1,23 @@
+public class CollegeNameAnalyzer {
+    public static void main(String[] args) {
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        for (String name : args) {
+            if (Character.isDigit(name.charAt(0))) {
+                System.out.println("Numbers are not allowed as names: " + name);
+                continue;
+            }
+
+            char firstChar = Character.toLowerCase(name.charAt(0));
+            if (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u') {
+                vowelCount++;
+            } else {
+                consonantCount++;
+            }
+        }
+
+        System.out.println("Number of names starting with a vowel: " + vowelCount);
+        System.out.println("Number of names starting with a consonant: " + consonantCount);
+    }
+}
