8abb32643a720faf9612a29e141008cdb7dc748e
XD777 <67903668+XD777OP@users.noreply.github.com>
2024-02-13 23:05:24 +0530

    Added College Learn Codes

---
+++ src/SumOddRange.java
@@ -0,0 +1,26 @@
+public class SumOddRange {
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1, 100)); // Example usage
+    }
+
+    public static boolean isOdd(int number) {
+        if (number <= 0) {
+            return false;
+        }
+        return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start <= 0 || end <= 0 || end < start) {
+            return -1;
+        }
+
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
